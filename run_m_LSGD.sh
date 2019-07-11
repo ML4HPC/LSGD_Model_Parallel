1feacb0a4b73abadb27b53d58c22201b9778f171
Yan Zhang <yzhanglearning>
2019-07-11 10:46:15 -0700

    run script for cori

--- run_m_LSGD.sh
+++ run_m_LSGD.sh
@@ -1,1 +1,6 @@
-srun python m_LSGD.py -a resnet50 --epoch 100 --batch-size 64 --gpu-num 4 --lr 6.4 /global/cscratch1/sd/kwangmin/dataset/ImageNet/ILSVRC2012
+#!/bin/sh
+
+#srun python _LSGD.py -a resnet50 --epoch 100 --batch-size 64 --gpu-num 8 --lr 6.4 /global/cscratch1/sd/kwangmin/dataset/ImageNet/ILSVRC2012
+
+
+srun python m_LSGD.py -a resnet50 --epoch 90 --batch-size 32 --train-workers 7 --lr 0.1 /global/cscratch1/sd/kwangmin/dataset/ImageNet/ILSVRC2012 
